81e6390ffde2a1417793333981c0dc6bf0643ca4
Victor2005l <sebastiantavarez532@gmail.com>
2025-06-13 19:53:54 -0400

    Tutorial #5
    Tutorial #5

---
+++ Segundo proyec/Segundo proyec/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Segundo_proyec
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNombre = new Label();
+            txtNombre = new TextBox();
+            btnSaludo = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(161, 291);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(64, 20);
+            lblNombre.TabIndex = 0;
+            lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(253, 195);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 1;
+            // 
+            // btnSaludo
+            // 
+            btnSaludo.Location = new Point(420, 187);
+            btnSaludo.Name = "btnSaludo";
+            btnSaludo.Size = new Size(152, 35);
+            btnSaludo.TabIndex = 3;
+            btnSaludo.Text = "Saludooo";
+            btnSaludo.UseVisualStyleBackColor = true;
+            btnSaludo.Click += btnSaludo_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(161, 202);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(btnSaludo);
+            Controls.Add(txtNombre);
+            Controls.Add(lblNombre);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private Button btnSaludo;
+        private Label label1;
+    }
+}
