0e5d92bb998db810a2254477ea0b88aba1e37505
Victor2005l <sebastiantavarez532@gmail.com>
2025-06-16 16:20:39 -0400

    Video #9
    Video #9

---
+++ Video #9/Video #9/Form1.cs
@@ -0,0 +1,19 @@
+namespace Video__9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            Form2 miforma = new Form2();
+
+            miform.ShowDialog();
+        }
+        {
+
+        }
+    }
+}
